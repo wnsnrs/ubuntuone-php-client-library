de433f03d1f27c2d0637a8e09a02690fc96379e0
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2011-08-17 21:13:28 +0200

    Comments added

--- apis/u1_NewApp.php
+++ apis/u1_NewApp.php
@@ -2,8 +2,8 @@
 
 class UbuntuOneNewApp{
 	
-	public $login_api_url = 'https://login.ubuntu.com/api/1.0/authentications?ws.op=authenticate&token_name=Ubuntu%20One%20@%20';
-	public $tell_api_url = 'https://one.ubuntu.com/oauth/sso-finished-so-get-tokens/';
+	public $login_api_url = 'https://login.ubuntu.com/api/1.0/authentications?ws.op=authenticate&token_name=Ubuntu%20One%20@%20'; //url to get tokens
+	public $tell_api_url = 'https://one.ubuntu.com/oauth/sso-finished-so-get-tokens/'; //url to tell Ubuntu One about newly created app
 	
 	public $app_name;
 		
@@ -16,16 +16,16 @@ class UbuntuOneNewApp{
 	public $token_secret;
 	
 
-	public function __construct($user_email, $user_pwd, $app_name){ //variabii $user_email o $user_pwd se non danno problemi perch&egrave; uguali a sopra sono da usare
+	public function __construct($user_email, $user_pwd, $app_name){ //construct function which require user email and password
 		
 		$this->user_email = $user_email;
 		$this->user_pwd = $user_pwd;
 		$this->app_name = $app_name;
 		}
 	
-	public function getTokens(){
+	public function getTokens(){ //functions to get tokens from Ubuntu One using curl
 		
-		$app_name_e = rawurlencode($this->app_name); //codifica app_name
+		$app_name_e = rawurlencode($this->app_name);
 		
 		$curl = curl_init($this->login_api_url.$app_name_e);
 		curl_setopt($curl, CURLOPT_USERPWD, $this->user_email.':'.$this->user_pwd);
@@ -37,12 +37,12 @@ class UbuntuOneNewApp{
 		
 		$response_d = json_decode($response);
 		
-		$this->conskey = $response_d->consumer_key;
-		$this->conssec = $response_d->consumer_secret;
-		$this->token = $response_d->token;
-		$this->token_secret = $response_d->token_secret;
+		$this->conskey = $response_d->consumer_key; //set consumer key
+		$this->conssec = $response_d->consumer_secret; //set consumer secret
+		$this->token = $response_d->token; //set token
+		$this->token_secret = $response_d->token_secret; //set token secret
 		
-		$this->app_name = $response_d->name; 
+		$this->app_name = $response_d->name; //set app name
 		
 		}
 
